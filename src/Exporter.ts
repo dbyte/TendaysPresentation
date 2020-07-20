7243a7b34dacc903d4111c03245600e00a92291a
T. Fornalik <it-github@fornalik.de>
2020-07-20 15:19:58 +0200

    Added module Utilities

--- src/Exporter.ts
+++ src/Exporter.ts
@@ -3,6 +3,7 @@ The purpose of this file is to avoid cyclic imports.
 Use this as the source of truth for importing other modules.
 **/
 export * from "./App.js";
+export * from "./Utilities.js";
 export * from "./PlayButton.js";
 export * from "./FullscreenButton.js";
 export * from "./Player.js";
--- src/PlayButton.ts
+++ src/PlayButton.ts
@@ -1,39 +1,4 @@
-import { Player } from "./Exporter.js";
-
-export enum CssAnimationEvent {
-    OnDocumentLoaded = "onLoadAnimate",
-    OnMouseOver = "onMouseoverAnimate",
-    OnMouseOut = "onMouseoutAnimate",
-    OnClick = "onClickAnimate"
-}
-
-export class CssAnimation {
-    private readonly element: HTMLElement;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-    }
-
-    public start(cssEvent: string): void {
-        this.removeAll();
-        this.element.classList.add(cssEvent);
-    }
-
-    public removeAll(): void {
-        const animationsToRemove: string[] = this.enumValuesToStringArray(CssAnimationEvent);
-        this.element.classList.remove(...animationsToRemove);
-    }
-
-    private enumValuesToStringArray(theEnum: typeof CssAnimationEvent): string[] {
-        var stringValues: string[] = [];
-        // This is a polyfill for Pre-ES7 clients. We don't have Object.values in pre-ES7.
-        for (let key of Object.keys(theEnum)) {
-            // @ts-ignore
-            stringValues.push(theEnum[key]);
-        }
-        return stringValues;
-    }
-}
+import { Player, CssAnimation, CssAnimationEvent } from "./Exporter.js";
 
 export class PlayButton {
     private readonly elemID: string;
