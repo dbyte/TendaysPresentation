501f0e65cf69a21904fda2e955bdc9ebaa9ec255
T. Fornalik <it-github@fornalik.de>
2020-07-20 15:13:42 +0200

    Added file

--- src/PlayButton.ts
+++ src/PlayButton.ts
@@ -1,9 +1,45 @@
 import { Player } from "./Exporter.js";
 
+export enum CssAnimationEvent {
+    OnDocumentLoaded = "onLoadAnimate",
+    OnMouseOver = "onMouseoverAnimate",
+    OnMouseOut = "onMouseoutAnimate",
+    OnClick = "onClickAnimate"
+}
+
+export class CssAnimation {
+    private readonly element: HTMLElement;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+    }
+
+    public start(cssEvent: string): void {
+        this.removeAll();
+        this.element.classList.add(cssEvent);
+    }
+
+    public removeAll(): void {
+        const animationsToRemove: string[] = this.enumValuesToStringArray(CssAnimationEvent);
+        this.element.classList.remove(...animationsToRemove);
+    }
+
+    private enumValuesToStringArray(theEnum: typeof CssAnimationEvent): string[] {
+        var stringValues: string[] = [];
+        // This is a polyfill for Pre-ES7 clients. We don't have Object.values in pre-ES7.
+        for (let key of Object.keys(theEnum)) {
+            // @ts-ignore
+            stringValues.push(theEnum[key]);
+        }
+        return stringValues;
+    }
+}
+
 export class PlayButton {
     private readonly elemID: string;
     private readonly pathToVideo: string;
-    private readonly buttonImageSource: string
+    private readonly buttonImageSource: string;
+    private cssAnimation: CssAnimation;
     private readonly player: Player;
     private animationEndMethod?: any;
 
@@ -11,61 +47,21 @@ export class PlayButton {
         this.elemID = elemID;
         this.pathToVideo = pathToVideo;
         this.buttonImageSource = "assets/play-button-grey.svg";
+        this.cssAnimation = new CssAnimation(this.getDomElem());
         this.player = new Player();
         this.animationEndMethod = undefined;
-        this.initDom();
+
+        this.initView();
         this.addEventListeners();
     }
 
-    public static get DomClassRegistry(): any {
-        const dflt: string[] = ["button-player"];
-        return {
-            DEFAULT: dflt,
-            ON_LOAD: dflt.concat(["onLoadAnimate"]),
-            ON_MOUSEOVER: dflt.concat(["onMouseoverAnimate"]),
-            ON_MOUSEOUT: dflt.concat(["onMouseoutAnimate"]),
-            ON_CLICK: dflt.concat(["onClickAnimate"])
-        };
-    }
-
-    private initDom(): void {
+    private initView(): void {
         let elem: HTMLVideoElement = this.getDomElem() as HTMLVideoElement;
         elem.src = this.buttonImageSource;
+        elem.classList.add("button-player");
     }
 
-    private addEventListeners(): void {
-        const events = ["load", "click", "mouseover", "mouseout"];
-        events.map(e => { return this.getDomElem()?.addEventListener(e, this) });
-    }
-
-    private removeEventListeners(): void {
-        const events = ["load", "click", "mouseover", "mouseout"];
-        events.map(e => { return this.getDomElem()?.removeEventListener(e, this) });
-    }
-
-    // Called JS-internally by the added listeners!
-    public handleEvent(e: Event): void {
-        switch (e.type) {
-            case "load":
-                console.log('load event called');
-                this.animateOnLoad();
-                break;
-            case "click":
-                console.log('click event called');
-                this.player.start(this.pathToVideo);
-                break;
-            case "mouseover":
-                console.log('mouseover event called');
-                this.animateOnMouseover();
-                break;
-            case "mouseout":
-                console.log('mouseout event called');
-                this.animateOnMouseout();
-                break;
-        }
-    }
-
-    private getDomElem(): HTMLElement | null {
+    private getDomElem(): HTMLElement | never {
         const elem = document.getElementById(this.elemID);
         if (elem != null) {
             return elem;
@@ -77,49 +73,75 @@ export class PlayButton {
         }
     }
 
-    private setDomClass(classes: string[]): void {
-        let elem = this.getDomElem();
-        elem?.removeAttribute("class");
-        elem?.classList.add(...classes);
+    private addEventListeners(): void {
+        const events = ["load", "click", "mouseover", "mouseout"];
+        events.map(e => { this.getDomElem().addEventListener(e, this) });
+    }
+
+    private removeEventListeners(): void {
+        const events = ["load", "click", "mouseover", "mouseout"];
+        events.map(e => { this.getDomElem().removeEventListener(e, this) });
+    }
+
+    // Called JS-internally by the added listeners!
+    public handleEvent(e: Event): void {
+        switch (e.type) {
+            case "load":
+                this.animateOnLoad();
+                break;
+            case "click":
+                console.log('click event called');
+                this.player.start(this.pathToVideo);
+                break;
+            case "mouseover":
+                this.animateOnMouseover();
+                break;
+            case "mouseout":
+                this.animateOnMouseout();
+                break;
+        }
     }
 
     private animateOnLoad(): void {
-        this.getDomElem()?.addEventListener("animationend", this.animationEndMethod = this.onLoadAnimationEnd.bind(this));
-        this.setDomClass(PlayButton.DomClassRegistry.ON_LOAD);
+        console.log('load event called');
+        this.getDomElem().addEventListener("animationend", this.animationEndMethod = this.onLoadAnimationEnd.bind(this));
+        this.cssAnimation.start(CssAnimationEvent.OnDocumentLoaded);
     }
     private onLoadAnimationEnd(e: Event): void {
         console.log('onAnimationEnd called');
-        this.setDomClass(PlayButton.DomClassRegistry.DEFAULT);
-        this.getDomElem()?.removeEventListener("animationend", this.animationEndMethod);
+        this.cssAnimation.removeAll();
+        this.getDomElem().removeEventListener("animationend", this.animationEndMethod);
     }
 
     private animateOnMouseover(): void {
-        this.getDomElem()?.addEventListener("animationend", this.animationEndMethod = this.onMouseoverAnimationEnd.bind(this));
-        this.setDomClass(PlayButton.DomClassRegistry.ON_MOUSEOVER);
+        console.log('mouseover event called');
+        this.getDomElem().addEventListener("animationend", this.animationEndMethod = this.onMouseoverAnimationEnd.bind(this));
+        this.cssAnimation.start(CssAnimationEvent.OnMouseOver);
     }
     private onMouseoverAnimationEnd(e: Event): void {
         console.log('onMouseoverAnimationEnd called');
-        this.getDomElem()?.removeEventListener("animationend", this.animationEndMethod);
+        this.getDomElem().removeEventListener("animationend", this.animationEndMethod);
     }
 
     private animateOnMouseout(): void {
-        this.getDomElem()?.addEventListener("animationend", this.animationEndMethod = this.onMouseoutAnimationEnd.bind(this));
-        this.setDomClass(PlayButton.DomClassRegistry.ON_MOUSEOUT);
+        console.log('mouseout event called');
+        this.getDomElem().addEventListener("animationend", this.animationEndMethod = this.onMouseoutAnimationEnd.bind(this));
+        this.cssAnimation.start(CssAnimationEvent.OnMouseOut);
     }
     private onMouseoutAnimationEnd(e: Event): void {
         console.log('onMouseoutAnimationEnd called');
-        this.getDomElem()?.removeEventListener("animationend", this.animationEndMethod);
+        this.getDomElem().removeEventListener("animationend", this.animationEndMethod);
     }
 
     public animateOnClick(): void {
         this.removeEventListeners();
-        this.getDomElem()?.addEventListener("animationend", this.animationEndMethod = this.onClickAnimationEnd.bind(this));
-        this.setDomClass(PlayButton.DomClassRegistry.ON_CLICK);
+        this.getDomElem().addEventListener("animationend", this.animationEndMethod = this.onClickAnimationEnd.bind(this));
+        this.cssAnimation.start(CssAnimationEvent.OnClick)
     }
     private onClickAnimationEnd(e: Event): void {
         console.log('onClickAnimationEnd called');
         const elem = this.getDomElem();
-        elem?.classList.add('invisible');
-        elem?.removeEventListener("animationend", this.animationEndMethod);
+        elem.classList.add('invisible');
+        elem.removeEventListener("animationend", this.animationEndMethod);
     }
 }
