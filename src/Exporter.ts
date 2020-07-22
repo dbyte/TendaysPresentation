f69bb0bc36685b2c9581f733a3a3fb9187dad37e
T. Fornalik <it-github@fornalik.de>
2020-07-22 13:45:49 +0200

    Removed .js suffix in exports

--- src/Exporter.ts
+++ src/Exporter.ts
@@ -2,9 +2,9 @@
 The purpose of this file is to avoid cyclic imports.
 Use this as the source of truth for importing other modules.
 **/
-export * from "./App.js";
-export * from "./Utilities.js";
-export * from "./OverlayHandler.js";
-export * from "./PlayButton.js";
-export * from "./FullscreenButton.js";
-export * from "./PlayerController.js";
+export * from "./App";
+export * from "./Utilities";
+export * from "./OverlayHandler";
+export * from "./PlayButton";
+export * from "./FullscreenButton";
+export * from "./PlayerController";
