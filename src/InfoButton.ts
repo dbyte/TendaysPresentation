34d7a9ec9c820eb8c0a225afeccd7a7d19f7c577
T. Fornalik <it-github@fornalik.de>
2020-07-24 01:41:00 +0200

    Animations for info button

--- src/InfoButton.ts
+++ src/InfoButton.ts
@@ -1,13 +1,17 @@
-import { Button } from "./Exporter";
+import { Button, CssAnimation, CssAnimationEvent } from "./Exporter";
 
 export class InfoButton extends Button {
+    private cssAnimation: CssAnimation;
+
     constructor() {
         super("infoButton01", "assets/info-button.svg");
+        this.cssAnimation = new CssAnimation(this.elem);
     }
 
     public initView(): void {
         super.initView();
-        this.elem.classList.add("player-navigation");
+        this.elem.classList.add("button-info");
+        this.animateIn();
     }
 
     // Called JS-internally by the added listeners!
@@ -18,5 +22,15 @@ export class InfoButton extends Button {
                 //ToDo
                 break;
         }
+    }
+
+    private animateIn(): void {
+        console.log('animateIn event called');
+        this.elem.addEventListener("animationend", () => { this.onInAnimationEnd() }, { once: true });
+        this.cssAnimation.start(CssAnimationEvent.ScaleIn);
+    }
+    private onInAnimationEnd(): void {
+        console.log('onInAnimationEnd called');
+        this.cssAnimation.removeAll();
     }
 }
