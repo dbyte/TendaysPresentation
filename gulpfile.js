008b70a1a9c846d4e5e8eb609be832e3b895edc9
T. Fornalik <it-github@fornalik.de>
2020-07-30 00:58:34 +0200

    Added production build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,60 @@
-const gulp = require('gulp');
-const merge = require('merge-stream');
-const browserify = require('browserify');
-const source = require('vinyl-source-stream');
-const tsify = require('tsify');
+/*
+This is Gulp's build file.
+**/
 
-gulp.task('copy-indexHtml', function () {
+const gulp = require('gulp'),
+    merge = require('merge-stream'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    tsify = require('tsify');
+
+class Helper {
+    static taskname = {
+        indexHtml: "copy-indexHtml",
+        views: "copy-views",
+        pwaRoots: "copy-pwa-roots"
+    }
+
+    static runParallelCopyTasks = () => {
+        return gulp.parallel(
+            Helper.taskname.indexHtml,
+            Helper.taskname.views,
+            Helper.taskname.pwaRoots);
+    }
+
+    static runBrowserify = (isDebug) => {
+        return browserify({
+            basedir: '.',
+            debug: isDebug,
+            entries: ['src/ts/App.ts'],
+            cache: {},
+            packageCache: {}
+        })
+        .plugin(tsify)
+        .bundle()
+    }
+}
+
+gulp.task(Helper.taskname.indexHtml, () => {
     return gulp.src('src/index.html')
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('copy-views', function () {
+gulp.task(Helper.taskname.views, () => {
     return gulp.src('src/views/**/*')
         .pipe(gulp.dest('dist/views'));
 });
 
-// These PWA files must go to the web-root to work properly.
-gulp.task('copy-pwa-roots', function () {
+gulp.task(Helper.taskname.pwaRoots, () => {
+    // These PWA files must go to the web-root to work properly.
     return gulp.src('src/pwa-root/*')
         .pipe(gulp.dest('dist'));
 });
 
-/* Copies the 'assets' directory into build directory.
+/* Task called by custom VSCode task!
+Copies the 'assets' directory into build directory.
 Rarely used, so it's not included in our standard build tasks. */
-gulp.task('copy-assets', function () {
+gulp.task('copy-assets', () => {
     const assetsDirectory = gulp.src(['src/assets/**/*', '!**/favicon.ico'])
         .pipe(gulp.dest('dist/assets'));
 
@@ -33,17 +65,18 @@ gulp.task('copy-assets', function () {
     return merge(assetsDirectory, favicon);
 });
 
-/* Builds a debug version without copying "assets" directory. */
-gulp.task('build-debug', gulp.series(gulp.parallel('copy-indexHtml', 'copy-views', 'copy-pwa-roots'), function () {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/ts/App.ts'],
-        cache: {},
-        packageCache: {}
-    })
-        .plugin(tsify)
-        .bundle()
+/* Task called by custom VSCode task!
+Builds a debug version without copying "assets" directory. */
+gulp.task('build-debug', gulp.series(Helper.runParallelCopyTasks(), () => {
+    return Helper.runBrowserify(true)
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist/js'));
+}));
+
+/* Task called by custom VSCode task!
+Builds a production version without copying "assets" directory. */
+gulp.task('build-production', gulp.series(Helper.runParallelCopyTasks(), () => {
+    return Helper.runBrowserify(false)
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('dist/js'));
 }));
