2c9fa5ce790049abb5d76b3bcf579e20bdbe8576
T. Fornalik <it-github@fornalik.de>
2020-07-29 05:45:03 +0200

    Refined support for Chrome desktop PWA

---
+++ pwa-serviceworker.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const CACHE_NAME = "tendays-presentation-cache-v1";
+
+const FILES_TO_CACHE = [
+    "index.html",
+    "views/css/app.css",
+    "views/css/spinner.css",
+    "views/hotspots-scene-01.html",
+    "views/main-navigation-bar.html",
+    "views/loading-spinner.html",
+
+    "assets/fullscreen-button.svg",
+    "assets/play-button-grey.svg",
+    "assets/reload-button.svg",
+    "assets/info-button.png",
+    "assets/homeScreen.jpg",
+
+    "assets/animation-01-medium.mp4",
+    "assets/animation-01-small.mp4",
+    "assets/animation-02-medium.mp4",
+    "assets/animation-02-small.mp4",
+    "assets/animation-03-medium.mp4",
+    "assets/animation-03-small.mp4"
+];
+
+self.addEventListener("install", (event) => {
+    // Precache static resources here.
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('ServiceWorker: Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log("ServiceWorker fetch event called");
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// self.addEventListener("fetch", (event) => {
+//     console.log("ServiceWorker adding event listener for fetch.");  
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then((response) => {
+//                 // Cache hit - return response
+//                 if (response) {
+//                     console.log("ServiceWorker returning cached response.");
+//                     return response;
+//                 }
+//                 console.log("ServiceWorker cache match but no response - fetching.");
+//                 return fetch(event.request);
+//             })
+//             .catch((reason) => {
+//                 throw new Error(`ServiceWorker cache match failed: ${reason}`);
+//             })
+//     );
+// });
--- src/App.ts
+++ src/App.ts
@@ -6,6 +6,11 @@ import { PlayerController } from "./Exporter";
 export class App {
     private static Singleton: App;
     private playerController?: PlayerController;
+    public static readonly IS_CLIENT_SAFARI =
+        navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
+        navigator.userAgent &&
+        navigator.userAgent.indexOf('CriOS') == -1 &&
+        navigator.userAgent.indexOf('FxiOS') == -1;
 
     private constructor() { }
 
@@ -22,12 +27,26 @@ export class App {
 
     public start() {
         window.onload = (() => {
+            if (!App.IS_CLIENT_SAFARI) this.registerServiceWorker();
             this.playerController = new PlayerController();
             this.playerController?.bindView()
-            .then(() => { this.playerController?.show() });
-            
+                .then(() => { this.playerController?.show() });
+
             console.log("PlayerController View initialized.");
         });
+    }
+
+    private registerServiceWorker() {
+        // This is for progressive web app (PWA) only
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('./pwa-serviceworker.js').then((registration) => {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, (err) => {
+                // registration failed :(
+                throw new Error(`ServiceWorker registration failed: ${err}`);
+            });
+        }
     }
 }
 
